26fa21362f788e4685eb2b6560adfff741539708
pingwoh <wordnerd@tx.rr.com>
2020-09-05 15:37:39 -0500

    ohhh my god
    this is vile

---
+++ Spektacles/Assets/Scripts/Pixies.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixies : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pixie interactions go here
+    }
+}
