cd33046070f9044894e6c3cf61e8c6cde39e50fd
JordanJenx23 <jordanaveryjenkins@yahoo.com>
2020-09-11 13:37:33 -0500

    Melita Sprite and Flooring
    Added in Melita Sprites
    Attempted to animate a walk cycle
    Added in old floor art

--- spektacles/Assets/Scripts/BasicMovement.cs
+++ spektacles/Assets/Scripts/BasicMovement.cs
@@ -6,36 +6,47 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     public int speed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Input.anyKey)
+        if (!Input.anyKey) //if nothing is pressed, don't move
         {
             rb.velocity = new Vector2(0, 0);
+            anim.Play("Melita_Idle");
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
+        //moving up
+        else if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
         {
             rb.velocity = new Vector2(0, speed);
+            anim.Play("Melita_Walk");
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        //moving down
+        else if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
         {
             rb.velocity = new Vector2(0, -speed);
+            anim.Play("Melita_Walk");
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        //moving left
+        else if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
         {
             rb.velocity = new Vector2(-speed, 0);
+            anim.Play("Melita_Walk");
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        //moving right
+        else if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
         {
             rb.velocity = new Vector2(speed, 0);
+            anim.Play("Melita_Walk");
         }
     }
 }
