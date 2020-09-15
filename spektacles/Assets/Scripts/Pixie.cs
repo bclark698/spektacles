d018214ac904f2de73050cffca53ff100a2fc9bb
zacgaler <zachary.galer@gmail.com>
2020-09-15 04:31:05 -0500

    Update Pixie.cs

--- spektacles/Assets/Scripts/Pixie.cs
+++ spektacles/Assets/Scripts/Pixie.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Pixie : MonoBehaviour
 {
-    // different states for enemy
+    ////////////
+    // STATES //
+    ////////////
     private enum State
     {
         Waiting,        // waiting to see enemy
@@ -17,11 +19,13 @@ public class Pixie : MonoBehaviour
     // PRIVATE VARIABLES //
     ///////////////////////
     private State state;            // current state of enemy (chasing, roaming, etc)
-    private Vector3 startingPos;    // pixies' starting position
-    private RaycastHit2D hitInfo;   // field of view object
+    private Vector2 startingPos;    // pixies' starting position
+    private RaycastHit2D hitInfo;   // ray that searches for player
+    private RaycastHit2D wallHit;   // ray that looks for walls / blocking objects
     private GameObject playerObj;   // player object (melita)
-    private Transform playerLoc;    // player location (melita)
     private bool playerHit;         // true if player has been hit, false if not
+    private bool atHome;            // true if pixies are at starting position
+    //private SpriteRenderer spriteRenderer; // used to flip sprite depending on movement direction
 
 
     ///////////////////////////////////////
@@ -34,7 +38,6 @@ public class Pixie : MonoBehaviour
     public AudioSource giggle2;
 
 
-
     ////////////////////
     // MAIN FUNCTIONS //
     ////////////////////
@@ -43,6 +46,7 @@ public class Pixie : MonoBehaviour
     private void Awake()
     {
         state = State.Waiting; // pixie's begin roaming upon level start
+        //this.spriteRenderer = this.GetComponent<SpriteRenderer>(); // get spriteRenderer component
     }
 
     // Start is called before the first frame update
@@ -50,62 +54,38 @@ public class Pixie : MonoBehaviour
     {
         Physics2D.queriesStartInColliders = false; // stops ray from detecting pixies own collider
         startingPos = transform.position; // gets pixie's starting position
+        atHome = true;
         playerObj = GameObject.FindGameObjectWithTag("Player"); // create player object
-        playerLoc = playerObj.GetComponent<Transform>(); // find player location
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //this.spriteRenderer.flipX = playerObj.transform.position.x < this.transform.position.x;
         // casts ray starting at transform.pos; casts in direction transform.right; length of ray = fovDistance
         hitInfo = Physics2D.Raycast(transform.position, transform.right, fovDistance);
 
-        switch(state)
+        switch (state)
         {
-            default:
+
             // CASE 1
             // Rotate. If player found, change state to chasing target.
+            default:
             case State.Waiting:
-                rotate();
-                if (playerSightCheck() == true)
-                {
-                    state = State.ChaseTarget;
-                    giggle1.Play();
-                }
+                lookForPlayer();
                 break;
 
             // CASE 2
             // Chase target until line of sight is broken or power-up is used.
             case State.ChaseTarget:
                 chaseTarget();
-                if(nonPlayerSightCheck() == true) // CURRENTLY DOESN'T WORK
-                {
-                    state = State.ReturnToStart;
-                    giggle2.Play();
-                    Debug.Log("ha ha ");
-                }
-                if(playerHit == true)
-                {
-                    state = State.ReturnToStart;
-                }
-                //if(powerUpUsed)
-                //{
-                //    state = State.ReturnToStart;
-                //}
                 break;
 
             // CASE 3
             // Return to start if line of sight is broken or power-up is used.
             case State.ReturnToStart:
-                returnToStart();
-                if(transform.position == startingPos)
-                {
-                    state = State.Waiting;
-
-                }
-
+                StartCoroutine(returnHome());
                 break;
         }
 
@@ -117,69 +97,118 @@ public class Pixie : MonoBehaviour
     //////////////////////
 
     // check if player is in line of sight
-    private bool playerSightCheck()
+    private bool canSeePlayer()
     {
         bool collisionCheck = false;
-
-        //// must add collider for walls and player
         if (hitInfo.collider != null)
         {
             Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                collisionCheck = true;
+                atHome = false;
+            }
         }
         else
         {
             Debug.DrawLine(transform.position, transform.position + transform.right * fovDistance, Color.green);
-        }
-
-        if(hitInfo.collider.CompareTag("Player"))
-        {
-            // player found, need to chase
-            collisionCheck = true;
+            collisionCheck = false;
         }
         return collisionCheck;
     }
 
     // check if pixie loses sight of player
-    private bool nonPlayerSightCheck()
+    private bool cantSeePlayer()
     {
         bool collisionCheck = false;
-
-        if (hitInfo.collider.CompareTag("Wall")) //FIX THIS; needs to check for any non player objects (walls, tables, doors)
+        if (wallHit.collider != null)
         {
-            // player lost, need to stop chasing
-            collisionCheck = true;
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            if (hitInfo.collider.CompareTag("Wall")) //FIX THIS; needs to check for any non player objects (walls, tables, doors)
+            {
+                collisionCheck = true;
+            }
         }
-
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right * fovDistance, Color.green);
+            collisionCheck = false;
+        }
         return collisionCheck;
     }
 
     // check if pixies touches player
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             playerHit = true;
             giggle2.Play();
         }
     }
 
-    // rotate pixies at rotationSpeed
-    private void rotate()
+
+    // searches for player
+    private void lookForPlayer()
     {
-        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        if (!canSeePlayer())
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            giggle1.Play();
+            state = State.ChaseTarget;
+            //Debug.Log("Chasing Target");
+        }
     }
 
     // causes pixies to chase player
     private void chaseTarget()
     {
+        wallHit = Physics2D.Raycast(transform.position, transform.right, fovDistance);
         // move towards the player using Vector2.MoveTowards(fromPosition, toPosition, speed);
-        transform.position = Vector2.MoveTowards(transform.position, playerLoc.position, moveSpeed * Time.deltaTime);
+        if (!playerHit)
+        {
+            // causes pixies to continously look towards player
+            Vector3 direction = playerObj.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(2, 2, angle);
+
+            transform.position = Vector2.MoveTowards(transform.position, playerObj.GetComponent<Transform>().position, moveSpeed * Time.deltaTime);
+            if (cantSeePlayer())
+            {
+                state = State.ReturnToStart;
+            }
+        }
+        else
+        {
+            state = State.ReturnToStart;
+            playerHit = false;
+        }
+        //if(nonPlayerSightCheck()) // CURRENTLY DOESN'T WORK
+        //{
+        //    state = State.ReturnToStart;
+        //    giggle2.Play();
+        //    Debug.Log("ha ha ");
+        //}
+        //if(powerUpUsed)
+        //{
+        //    state = State.ReturnToStart;
+        //}
     }
 
     // sends pixies back to starting position
-    private void returnToStart()
+    IEnumerator returnHome()
     {
-        transform.position = Vector2.MoveTowards(transform.position, startingPos, moveSpeed * Time.deltaTime);
+        float reachedPosDist = 1f;
+        while (Vector2.Distance(transform.position, startingPos) > reachedPosDist)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, startingPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        atHome = true;
+        state = State.Waiting;
     }
 
 }
