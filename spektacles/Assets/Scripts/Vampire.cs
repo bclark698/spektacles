bed6f12b35bb2029ce1dff5c39d720fa093aba9b
pingwoh <wordnerd@tx.rr.com>
2020-09-22 21:28:30 -0500

    fungus scene :)
    u know what fungus is a cute name

--- spektacles/Assets/Scripts/Vampire.cs
+++ spektacles/Assets/Scripts/Vampire.cs
@@ -20,6 +20,8 @@ public class Vampire : Enemy
         // mark as stunned for a few seconds
         isStunned = true;
 
+        //might be good to add a specific animation for the vampires to play
+
         // wait for 1.5 seconds
         yield return new WaitForSeconds(1.5f);
 
