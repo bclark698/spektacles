bc8ecef8be4da8ec3e121004e92eed07606d2488
pingwoh <wordnerd@tx.rr.com>
2020-09-23 00:45:55 -0500

    fixed sirens

---
+++ spektacles/Assets/Scripts/Gravity.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    private Rigidbody2D playerRB; //var for player rigidbody
+    [HideInInspector]
+    public bool inRange = false; //if player is in range
+    [HideInInspector]
+    public CircleCollider2D gravityRange;
+
+    [SerializeField] private float gravitationalForce = 90; //adjust gravity
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>(); //grab player RB
+        gravityRange = gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (inRange)
+        {
+            // make a vector 2 of wherever the player is
+            var directionOfPlayer = ((Vector2)transform.position - playerRB.position).normalized;
+
+            // adds force to the player to pull them towards siren
+            playerRB.AddForce(directionOfPlayer * gravitationalForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //once u enter the range
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = true; //start pulling player
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) //once u leave the range
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = false; //stop pulling player
+        }
+    }
+}
--- spektacles/Assets/Scripts/Sirens.cs
+++ spektacles/Assets/Scripts/Sirens.cs
@@ -4,47 +4,8 @@ using UnityEngine;
 
 public class Sirens : Enemy
 {
-    private Rigidbody2D playerRB; //var for player rigidbody
-    private bool inRange = false; //if player is in range
-
-    [SerializeField] private float gravitationalForce = 5; //adjust gravity
     private PowerUp.PowerUpType sirenPowerUp = PowerUp.PowerUpType.EarPlugs;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>(); //grab player RB
-    }
-
-    private void FixedUpdate()
-    {
-        if (inRange)
-        {
-            // make a vector 2 of wherever the player is
-            var directionOfPlayer = ((Vector2)transform.position - playerRB.position).normalized;
-
-            // adds force to the player to pull them towards siren
-            playerRB.AddForce(directionOfPlayer * gravitationalForce);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) //once u enter the range
-    {
-        if(collision.CompareTag("Player") && !isStunned)
-        {
-            inRange = true; //start pulling player
-        }
-
-    }
-
-    private void OnTriggerExit2D(Collider2D collision) //once u leave the range
-    {
-        if (collision.CompareTag("Player") && !isStunned)
-        {
-            inRange = false; //stop pulling player
-        }
-    }
-
+    public Gravity pull;
 
     public override void HandlePowerUp(PowerUp.PowerUpType powerUp)
     {
@@ -58,13 +19,13 @@ public class Sirens : Enemy
     public override IEnumerator HandleStun()
     {
         isStunned = true; //mark as stunned
-        inRange = false; //mark player out of range (no continuing to pull)
-        gameObject.GetComponent<CircleCollider2D>().enabled = false; //turn off range
+        pull.inRange = false; //mark player out of range (no continuing to pull)
+        pull.gravityRange.enabled = false; //turn off range
 
         // wait for 5 seconds - long enough to get across range
         yield return new WaitForSeconds(5f);
 
-        gameObject.GetComponent<CircleCollider2D>().enabled = true; //turn on range
+        pull.gravityRange.enabled = true; //turn on range
         isStunned = false; //no longer stunned
     }
 
