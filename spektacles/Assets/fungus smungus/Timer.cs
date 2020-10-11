74a0147c1e86bca189dc6cc173b8c9d960b4d152
pingwoh <wordnerd@tx.rr.com>
2020-10-10 20:29:09 -0500

    timer functionality
    needs to be moved around

---
+++ spektacles/Assets/fungus smungus/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    private bool coolingDown;
+    [SerializeField] private Text timerText;
+    [SerializeField] private Image ability;
+    private float elapsed;
+
+    private void Update()
+    {
+        //currently its own script but could be handled by the player entirely
+        if(Input.GetKeyDown(KeyCode.Q) && !coolingDown)
+        {
+            print("did a thing");
+            coolingDown = true;
+            timerText.text = "cooling down";
+            ability.fillAmount = 1;
+        }
+
+        if(coolingDown)
+        {
+            elapsed += Time.deltaTime;
+            ability.fillAmount -= 1 / 3f * Time.deltaTime;
+            if(elapsed >= 3f)
+            {
+                coolingDown = false;
+                elapsed = 0;
+                timerText.text = "cooldown finished";
+            }
+        }
+
+    }
+
+}
