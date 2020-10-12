060fd56ead1fff8ddd6b5d5ff71464767bcfd244
Courtney <qwertyhwin@gmail.com>
2020-10-12 14:06:50 -0500

    fungus block executing from external script to allow interactions with the new input system

---
+++ spektacles/Assets/Scripts/FungusInteract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Fungus;
+
+public class FungusInteract : MonoBehaviour
+{
+    public PlayerControls controls;
+    //public BlockReference blockRef;
+    public Flowchart flowchart;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Gameplay.EquipOrInteract.performed += _ => flowchart.ExecuteBlock("Talk to Mom"); ;
+    }
+
+    // Called when the Player object is enabled
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
