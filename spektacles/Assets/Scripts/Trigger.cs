0d750c4205eb46e050d156579b7ba58cf7296a81
Courtney <qwertyhwin@gmail.com>
2020-10-16 11:54:05 -0500

    petrify indicator trigger in bus

---
+++ spektacles/Assets/Scripts/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+	[SerializeField] private string tag = "Player";
+	[SerializeField] private ShowInteractIndicator.Icon icon;
+
+    void OnTriggerEnter2D(Collider2D other) {
+    	if(other.CompareTag(tag)) {
+    		other.gameObject.GetComponent<ShowInteractIndicator>().Show(icon);
+    	}
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+    	if(other.CompareTag(tag)) {
+    		other.gameObject.GetComponent<ShowInteractIndicator>().Hide();
+    	}
+    }
+}
