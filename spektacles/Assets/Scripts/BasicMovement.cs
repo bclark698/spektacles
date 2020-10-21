73ce691b42c9d4f8762d8692e306203e0394d239
Soumya Kovela <soumyakovela@utexas.edu>
2020-10-20 21:07:40 -0500

    bus background and momdusa sprite update
    updated bus bg and momdusa is now cropped like the rest of the character sprites

---
+++ spektacles/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    public int speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.anyKey) //if nothing is pressed, don't move
+        {
+            rb.velocity = new Vector2(0, 0);
+            anim.Play("Melita_Idle");
+        }
+        //moving up
+        else if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
+        {
+            rb.velocity = new Vector2(0, speed);
+            anim.Play("Melita_Walk");
+        }
+        //moving down
+        else if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
+        {
+            rb.velocity = new Vector2(0, -speed);
+            anim.Play("Melita_Walk");
+        }
+        //moving left
+        else if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
+        {
+            rb.velocity = new Vector2(-speed, 0);
+            anim.Play("Melita_Walk");
+        }
+        //moving right
+        else if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
+        {
+            rb.velocity = new Vector2(speed, 0);
+            anim.Play("Melita_Walk");
+        }
+    }
+}
---
+++ spektacles/Assets/glassesOffTemp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glassesOffTemp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(){
+      gameObject.SetActive(false)
+;    }
+}
