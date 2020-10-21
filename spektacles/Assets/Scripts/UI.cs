50c74a2a54d7891380a6158e736e693211ad8057
Sohee Na <sysm06@gmail.com>
2020-10-21 17:09:05 -0500

    ui changes
    timer

---
+++ spektacles/Assets/Scripts/UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Image theButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        theButton.alphaHitTestMinimumThreshold = 0.5f;
+    }
+}
