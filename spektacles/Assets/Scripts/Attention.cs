c62efe81b5b33208345a0fac63844193c6b32f43
Courtney Huynh <32528336+qwertyhwin@users.noreply.github.com>
2020-11-06 00:17:46 -0600

    some resource loading for attention and tip popups

--- spektacles/Assets/Scripts/Attention.cs
+++ spektacles/Assets/Scripts/Attention.cs
@@ -24,6 +24,7 @@ public class Attention : MonoBehaviour
 		if(attentionCleared == null) {
 			attentionCleared = new UnityEvent();
 		}
+		interactSprite = PlatformSpecific.instance.speechInteract;
 	}
 
     void HideOnEnter() {
