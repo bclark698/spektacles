ab59d2a00d0b2f7995c74abaa8f604fa12d4b53c
bclark698 <60580597+bclark698@users.noreply.github.com>
2020-11-06 18:08:37 -0600

    Tip pop-up in lvl 1

---
+++ spektacles/Assets/Tip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tip : MonoBehaviour
+{
+
+    private GameObject tipObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  tipObject = GameObject.Find("Tip");
+      tipObject = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+      if (col.gameObject.CompareTag("Player"))
+       {
+            tipObject.SetActive(true);
+       }
+    }
+
+
+      void OnTriggerExit2D(Collider2D col)
+    {
+      if (col.gameObject.CompareTag("Player"))
+       {
+            tipObject.SetActive(false);
+       }
+
+    }
+}
