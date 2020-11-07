3e02ec106214666f348e7e8094369feee6baea08
Courtney Huynh <32528336+qwertyhwin@users.noreply.github.com>
2020-11-07 00:10:40 -0600

    reorganizing assets

--- spektacles/Assets/Scripts/Module.cs
+++ spektacles/Assets/Scripts/Module.cs
@@ -9,13 +9,17 @@ public class Module : MonoBehaviour
     		Debug.Log("module enter!");
     	}
     	if(other.CompareTag("End")) {
-    		Debug.Log("irving in this module!");
+    		Debug.Log("irving in this module!"); // doesnt work
     	}
     }
 
     void OnTriggerStay2D(Collider2D other) {
     	if(other.CompareTag("End")) {
-    		Debug.Log("irving in module STAY");
+    		Debug.Log("irving in module STAY"); // doesnt work
     	}
+    }
+
+    void UpdateMiniMap() {
+    	//
     }
 }
