5b841a813f9f1a7d55a9b564c0de6baad2095af8
bclark698 <60580597+bclark698@users.noreply.github.com>
2020-11-13 14:58:40 -0600

    Toprac trigger & sound

---
+++ spektacles/Assets/TriggerAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+    private Animator animControl;
+    private AudioSource canSound;
+
+    void Start()
+    {
+        animControl = gameObject.GetComponent<Animator>();
+        canSound = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+      if (other.tag == "Player"){
+      animControl.SetTrigger("OpenCan");
+      canSound.Play();
+      }
+
+    }
+}
