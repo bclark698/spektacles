b31d83020067b88edda3f1e1e1ab0039db337680
zacgaler <zachary.galer@gmail.com>
2020-11-24 22:16:09 -0600

    Working VampSwitcher

--- spektacles/Assets/Scripts/Vampire.cs
+++ spektacles/Assets/Scripts/Vampire.cs
@@ -2,11 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vampire : Enemy
+public class Vampire : MonoBehaviour
 {
+    public GameObject[] vamps;
+    public int num = 0;
+    private int prevNum;
+
+    public float waitTime;
+    private float timer;
+
+    public float stunDuration;
+    public bool rand = false;
+    public bool go = true;
+
+
+
     void Start() {
-        powerUpToHandle = PowerUp.Type.Garlic;
+        //powerUpToHandle = PowerUp.Type.Garlic;
+
+        timer = waitTime;
+
+        for(int i = 0; i < vamps.Length; i++)
+        {
+            vamps[i].gameObject.SetActive(false);
+        }
     }
+
+    private void Update()
+    {
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            return;
+        }
+        timer = waitTime;
+
+        if (!rand)
+        {
+            if (num + 1 == vamps.Length)
+            {
+                prevNum = num;
+                num = 0;
+            }
+            else
+            {
+                prevNum = num;
+                num++;
+            }
+        }
+        else
+        {
+            prevNum = num;
+            num = Random.Range(0, vamps.Length);
+        }
+
+        vamps[prevNum].gameObject.SetActive(false);
+        vamps[num].gameObject.SetActive(true);
+
+        HandleStun();
+
+    }
+
+
+    public  IEnumerator HandleStun()
+    {
+        yield return new WaitForSeconds(stunDuration);
+    }
+
+}
+
+
+
 
     /*
     public override IEnumerator HandleStun()
@@ -21,4 +88,4 @@ public class Vampire : Enemy
 
         isStunned = false;
     }*/
-}
+
