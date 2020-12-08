7f26286c4ed2d4457f4a14df5d3a89264b1b751c
Courtney Huynh <32528336+qwertyhwin@users.noreply.github.com>
2020-12-07 23:36:49 -0600

    adjusting settings page a little bit (still unfinished)
    need to make resolution sliders and refine volume sliders

--- spektacles/Assets/MixerControl.cs
+++ spektacles/Assets/MixerControl.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class MixerControl : MonoBehaviour
 {
     public AudioMixer mixer;
+    [SerializeField] private TextMeshProUGUI musicPercentage;
+    [SerializeField] private TextMeshProUGUI sfxPercentage;
 
     public void setMusicLevel(float musicLvl){
       mixer.SetFloat("MusicVol", musicLvl);
+      Debug.Log("MUSIC LVL: "+musicLvl); // TODO remove- used for debugging
+      musicPercentage.text = "100%"; // TODO actually calculate
    }
 
    public void setSFXLevel(float sfxLvl){
      mixer.SetFloat("SFXVol", sfxLvl);
+     Debug.Log("SFX LVL: "+sfxLvl); // TODO remove- used for debugging
+     sfxPercentage.text = "100%"; // TODO actually calculate
    }
 }
