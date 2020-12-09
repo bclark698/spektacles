2ea6ac52aa81407244d9eb8ac431cc5cb1c60b55
bclark698 <60580597+bclark698@users.noreply.github.com>
2020-12-09 13:51:16 -0600

    SFX test noise

---
+++ spektacles/Assets/SFXTestSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFXTestSound : MonoBehaviour
+{
+
+  public GameObject slider;
+  public AudioSource testNoise;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //slider = gameObject.GetComponent<Slider>();
+      //slider.onValueChanged.AddListener (delegate {MouseUpCheck ();});
+    }
+    public void OnMouseDown(){
+    Debug.Log("Mouse Down");
+    }
+
+    public void MouseUpCheck(){
+      Debug.Log("Mouse up");
+      testNoise.Play();
+    }
+    void OnMouseUp(){
+      Debug.Log("Mouse Up");
+      testNoise.Play();
+    }
+
+    void OnMouseUpAsButton(){
+      Debug.Log("Mouse Up as button");
+      testNoise.Play();
+    }
+}
