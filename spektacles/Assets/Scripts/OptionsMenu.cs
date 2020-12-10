74c7e6ed5779a2c89384c2bad03c5c7d498efa51
pingwoh <wordnerd@tx.rr.com>
2020-12-10 15:47:38 -0600

    resolution

---
+++ spektacles/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public TMP_Dropdown resolution;
+
+    public void ChangeResolution()
+    {
+        switch (resolution.value)
+        {
+            case 0:
+                Screen.SetResolution(2560, 1600, false);
+                break;
+            case 1:
+                Screen.SetResolution(2048, 1280, false);
+                break;
+            case 2:
+                Screen.SetResolution(1440, 900, false);
+                break;
+            case 3:
+                Screen.SetResolution(1280, 800, false);
+                break;
+            case 4:
+                Screen.SetResolution(800, 600, false);
+                break;
+        }
+    }
+}
